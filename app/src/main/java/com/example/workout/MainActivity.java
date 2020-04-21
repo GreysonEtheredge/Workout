bc26046f57a3fc57ac3097b0eb9c6e37a7ad148b
waldron5 <waldron5@illinois.edu>
2020-04-21 14:09:38 -0500

    Working on Buttons

--- app/src/main/java/com/example/workout/MainActivity.java
+++ app/src/main/java/com/example/workout/MainActivity.java
@@ -3,6 +3,8 @@ package com.example.workout;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
+import android.widget.ToggleButton;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ToggleButton chestButton = (ToggleButton) findViewById(R.id.chestButton);
+        TextView tv = (TextView) findViewById(R.id.textView);
     }
 }
