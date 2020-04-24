fcfbf944fa3e96d721bca2c60677bfaf946babd3
waldron5 <waldron5@illinois.edu>
2020-04-24 14:47:20 -0500

    Fixed pull issues

--- app/src/main/java/com/example/workout/MainActivity.java
+++ app/src/main/java/com/example/workout/MainActivity.java
@@ -9,15 +9,33 @@ import android.widget.ImageButton;
 
 public class MainActivity extends AppCompatActivity {
 
+    private int type = 0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         ImageButton chest = (ImageButton) findViewById(R.id.imageButton2);
+        ImageButton back = (ImageButton) findViewById(R.id.imageButton);
+        ImageButton legs = (ImageButton) findViewById(R.id.imageButton3);
 
         chest.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                type = 1;
+                startActivity(new Intent(MainActivity.this,pop.class));
+            }
+        });
+        back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                type = 2;
+                startActivity(new Intent(MainActivity.this,pop.class));
+            }
+        });
+        legs.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                type = 3;
                 startActivity(new Intent(MainActivity.this,pop.class));
             }
         });
