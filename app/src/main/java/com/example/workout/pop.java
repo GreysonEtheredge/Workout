d57540aba9399224d832ec12ccb28c5d477123d0
gketh <gke3@illinois.edu>
2020-04-24 15:10:37 -0500

    commit

--- app/src/main/java/com/example/workout/pop.java
+++ app/src/main/java/com/example/workout/pop.java
@@ -2,12 +2,18 @@ package com.example.workout;
 
 import android.os.Bundle;
 import android.util.DisplayMetrics;
-import android.widget.NumberPicker;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class pop extends MainActivity {
+
+    EditText editText3;
+    int exerciseNumber = 0;
+    Button submitButton;
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        NumberPicker exercises = null;
         setContentView(R.layout.chestpopup);
 
         DisplayMetrics dm = new DisplayMetrics();
@@ -17,5 +23,14 @@ public class pop extends MainActivity {
         int height = dm.heightPixels;
 
         getWindow().setLayout((int)(width * 0.4), (int)(height * 0.4));
+
+        editText3 = (EditText) findViewById(R.id.editText3);
+        submitButton = (Button) findViewById(R.id.submitButton);
+        submitButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                exerciseNumber = Integer.valueOf(editText3.getText().toString());
+            }
+        });
     }
 }
