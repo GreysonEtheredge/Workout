d1d79d1f739a78cca2ce96eac7cd9635902c1db1
gketh <gke3@illinois.edu>
2020-04-25 21:37:18 -0500

    commit

---
+++ app/src/main/java/com/example/workout/BackExercises.java
@@ -0,0 +1,19 @@
+package com.example.workout;
+
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class BackExercises extends pop {
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.backex1);
+
+        TextView textView2 = (TextView) findViewById(R.id.textView2);
+        TextView textView3 = (TextView) findViewById(R.id.textView3);
+        TextView textView4 = (TextView) findViewById(R.id.textView4);
+        TextView textView5 = (TextView) findViewById(R.id.textView5);
+        TextView textView6 = (TextView) findViewById(R.id.textView6);
+        TextView textView7 = (TextView) findViewById(R.id.textView7);
+    }
+}
